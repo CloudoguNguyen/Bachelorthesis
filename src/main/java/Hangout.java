5039fefb98f63e20d82a7d7c47ee3449957299b6
CloudoguNguyen <thanh.long.nguyen@cloudogu.com>
2018-09-17 15:03:58 +0200

    Add Dialogflow.java

--- src/main/java/Hangout.java
+++ src/main/java/Hangout.java
@@ -1,10 +1,9 @@
-import com.fasterxml.jackson.core.JsonFactory;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.chat.v1.HangoutsChat;
 import com.google.api.services.plus.PlusScopes;
-import com.google.api.services.plus.Plus;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,18 +12,21 @@ import java.util.Collections;
 
 public class Hangout {
 
+    private HttpTransport httpTransport;
+    private JacksonFactory jsonFactory;
+    HangoutsChat chat;
+
     public Hangout() throws GeneralSecurityException, IOException {
-        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+         httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+         jsonFactory = JacksonFactory.getDefaultInstance();
 
         GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream("src/main/resources/intelliJ.json"))
                 .createScoped(Collections.singleton(PlusScopes.PLUS_ME));
 
-        Plus plus = new Plus.Builder(httpTransport, jsonFactory, credential)
-                .setApplicationName("test").build();
+        /*Plus plus = new Plus.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("test").build();*/
 
-        System.out.println(credential.getServiceAccountPrivateKeyId());
-
+        chat = new HangoutsChat.Builder(httpTransport,jsonFactory,null).build();
 
     }
 
